3a366a6e638a562f52b8c60436c2b0a5f51e5aa7
Danilo Yuudi <dnl496@outlook.com>
2020-10-20 21:27:01 -0300

    Aula04

---
+++ Aula04/exercicio1.R
@@ -0,0 +1,4 @@
+dataframe = data.frame(nome=c("Mario","Joana","Mario","Sara","Sofia","Mario"),
+                       idade=c(50,47,50,18,45,50),
+                       casado=c(1,0,0,1,0,0))
+unique(dataframe)
